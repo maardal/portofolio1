5505f01f0d1a9c1cf0c0932db2374065b8c50e10
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-11-17 09:12:26 +0100

    terraform-azure - change step

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -14,4 +14,10 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
+
+    tags = {
+        Environment = "Terraform Getting Started"
+        Team = "DevOps"
+    }
+
 }
